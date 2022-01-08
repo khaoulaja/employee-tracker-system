935cb8839c241aacd607c495abe4b3df30febc2e
khaoula <khaoulajabour4@gmail.com>
2022-01-08 03:05:51 -0500

    populate department and role tables

---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO department (name) values
+('Administration/operations'),
+('Research and development'),
+('Marketing and sales'),
+('Human resources'),
+('Customer service'),
+('Accounting and finance');
+
+INSERT INTO role(title, salary, department_id) values
+('Manager', 100.000, 1),
+('Engineer', 90.000, 1),
+('Manager', 130.000, 2),
+('Engineer', 110.000, 2),
+('Manager', 85.000, 3),
+('Engineer', 65.000, 3),
+('Manager', 90.000, 4),
+('Engineer', 70.000, 4),
+('Manager', 95.000, 5),
+('Engineer', 75.000, 5);
+
+
+
